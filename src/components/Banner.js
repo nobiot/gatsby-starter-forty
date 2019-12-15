8ffd83749a48b2b2eac8f7001c0b01b58fe3b327
nobiot <noboru.ota@gmail.com>
2019-12-15 18:41:25 +0100

    JLS

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
 const Banner = (props) => (
-    <section id="banner" className="major">
-        <div className="inner">
-            <header className="major">
-                <h1>Hi, my name is Forty</h1>
-            </header>
-            <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
-                <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
-                </ul>
-            </div>
-        </div>
-    </section>
+  <section id='banner' className='major'>
+    <div className='inner'>
+      <header className='major'>
+        <h1>自由が丘ランゲージスクールへようこそ。</h1>
+        <h2>自由が丘駅徒歩３０秒。</h2>
+        <p>伝統あるアットホームな英語・フランス語教室</p>
+      </header>
+      <div className='content'>
+        <p>お問合せ・お申込みは ０１２０－００９－０３５<br />(フリーダイアル)</p>
+        <ul className='actions'>
+          <li><a href='#one' className='button next scrolly'>英語</a></li>
+          <li><a href='#french' className='button next scrolly'>フランス語</a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
 )
 
 export default Banner
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
 const Header = (props) => (
-    <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-        </nav>
-    </header>
+  <header id='header' className='alt'>
+    <Link to='/' className='logo'><img src='../images/emblem_white.png' width='50'/></Link>
+    <nav>
+      <a className='menu-link' onClick={props.onToggleMenu} href='javascript:;'>Menu</a>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onToggleMenu: PropTypes.func
+  onToggleMenu: PropTypes.func
 }
 
 export default Header
