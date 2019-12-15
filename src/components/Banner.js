c7fa4aa98e1ddb94353169f8dfb572dd24ff9d23
nobiot <noboru.ota@gmail.com>
2019-12-15 19:11:08 +0100

    jls

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,13 +6,18 @@ const Banner = (props) => (
       <header className='major'>
         <h1>自由が丘ランゲージスクールへようこそ。</h1>
         <h2>自由が丘駅徒歩３０秒。</h2>
-        <p>伝統あるアットホームな英語・フランス語教室</p>
+        <h3>伝統あるアットホームな英語・フランス語教室</h3>
+        <p>お問合せ・お申込みは ０１２０－００９－０３５<br />(フリーダイアル)</p>
       </header>
       <div className='content'>
-        <p>お問合せ・お申込みは ０１２０－００９－０３５<br />(フリーダイアル)</p>
         <ul className='actions'>
-          <li><a href='#one' className='button next scrolly'>英語</a></li>
-          <li><a href='#french' className='button next scrolly'>フランス語</a></li>
+          <li><a href='#one' className='button next fit scrolly'>高校生 英語塾</a></li>
+        </ul>
+      </div>
+      <div className='content'>
+        <ul className='actions'>
+          <li><a href='#one' className='button  special scrolly'>英語</a></li>
+          <li><a href='#french' className='button  special scrolly'>フランス語</a></li>
         </ul>
       </div>
     </div>
