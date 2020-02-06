79ce5ed5a38de7cbd1e73414360099a0af7fc1cc
nobiot <noboru.ota@gmail.com>
2020-02-06 14:02:04 +0100

    adding netlify-cms-app

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: 'Gatsby Starter - Forty V2',
+    author: 'Hunter Chang',
+    description: 'A Gatsby.js V2 Starter based on Forty by HTML5 UP'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,10 +15,11 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/assets/images/website-icon.png' // This path is relative to the root of the site.
+      }
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
-  ],
+    'gatsby-plugin-offline',
+    'gatsby-plugin-netlify-cms'
+  ]
 }
